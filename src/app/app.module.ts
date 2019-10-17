3583728f625b5068f0eaa1ba05e192e5d3a59640
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-10-16 17:45:28 -0700

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
