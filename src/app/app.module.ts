5c7eea9a3d257e733e01ec10e35afdb9a31df122
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-10-31 21:29:19 -0700

    check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarCommonModule, CalendarMonthModule } from 'angular-calendar';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,10 @@ import { MainFooterComponent } from './main-footer/main-footer.component';
     MainFooterComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    CalendarCommonModule,
+    CalendarMonthModule,
+    NgbModule,
     BrowserModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
