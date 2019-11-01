54f046669545c7923fd3725226f42679b381095f
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-10-31 21:48:14 -0700

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarCommonModule, CalendarMonthModule } from 'angular-calendar';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
