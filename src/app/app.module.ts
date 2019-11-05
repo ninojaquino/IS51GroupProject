4634c30ab3b135cd10c84ddf846856e4ddd21b64
ninojaquino <ninojaquino@gmail.com>
2019-11-05 13:13:30 -0800

    add code for CRUD and string concatenation heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -9,7 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarCommonModule, CalendarMonthModule } from 'angular-calendar';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
@@ -21,10 +24,14 @@ import { CalendarCommonModule, CalendarMonthModule } from 'angular-calendar';
   ],
   imports: [
     BrowserAnimationsModule,
+    CommonModule,
     CalendarCommonModule,
     CalendarMonthModule,
     NgbModule,
+    NgbModalModule,
     BrowserModule,
+    FormsModule,
+    FlatpickrModule.forRoot(),
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
