51c26bc2b5e6547943535a6c0eff24aaa4772989
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-12-06 22:30:57 -0800

    changes to design

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -4,6 +4,8 @@ import { MatCheckboxModule } from '@angular/material';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [],
@@ -12,13 +14,15 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatCheckboxModule,
     MatButtonToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatDatepickerModule
   ],
   exports: [
     MatButtonModule,
     MatCheckboxModule,
     MatButtonToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatDatepickerModule
   ]
 })
 export class MaterialDesignModule { }
