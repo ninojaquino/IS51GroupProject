8482434d746d1ac461757717a0fbcda52f71dfde
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-12-09 17:36:31 -0800

    added errors to the code for Zodiac

--- src/app/zodiac-signs/zodiac-signs.component.ts
+++ src/app/zodiac-signs/zodiac-signs.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { MonthPickerComponent } from 'ngx-bootstrap';
 // import { Http } from '@angular/http';
-interface Izodiac {
+interface IZodiac {
   name: string;
   month: string;
   day: number;
@@ -19,19 +19,15 @@ export class ZodiacSignsComponent implements OnInit {
   public value: any;
   public month: any;
   public day: any;
-  // const zodiac: string;
   public zod: ZodiacSignsComponent;
-  zodiacs: Array<Izodiac> = [];
-  // nameInput = ' ';
+  zodiacs: Array<IZodiac> = [];
 
-  // date = '';
   constructor() {
   }
 
   ngOnInit() {
 
   }
-
 
 
   zodiac(month, day) {
@@ -45,75 +41,101 @@ export class ZodiacSignsComponent implements OnInit {
 
       if (day < 22) {
         this.value = 'Sagittarius';
-      } else {
+      } else if (day > 22 && day <= 31) {
         this.value = 'Capricorn';
+      } else {
+        this.value = 'Only 31 days in December!';
       }
     } else if (month === 'january') {
       if (day < 20) {
         this.value = 'Capricorn';
-      } else {
+      } else if (day > 20 && day <= 31) {
         this.value = 'Aquarius';
+      } else {
+        this.value = 'Only 31 days in January!';
       }
     } else if (month === 'february') {
       if (day < 19) {
         this.value = 'Aquarius';
-      } else {
+      } else if (day > 19 && day <= 29) {
         this.value = 'Pisces';
+      } else {
+        this.value = 'Only 29 days in February!';
       }
     } else if (month === 'march') {
       if (day < 21) {
         this.value = 'Pisces';
-      } else {
+      } else if (day > 21 && day <= 31) {
         this.value = 'Aries';
+      } else {
+        this.value = 'Only 31 days in March!';
       }
     } else if (month === 'april') {
       if (day < 20) {
         this.value = 'Aries';
-      } else {
+      } else if (day > 20 && day <= 30) {
         this.value = 'Taurus';
+      } else {
+        this.value = 'Only 30 days in April!';
       }
     } else if (month === 'may') {
       if (day < 21) {
         this.value = 'Taurus';
-      } else {
+      } else if (day > 21 && day <= 31) {
         this.value = 'Gemini';
+      } else {
+        this.value = 'Only 31 days in May!';
       }
     } else if (month === 'june') {
       if (day < 21) {
         this.value = 'Gemini';
-      } else {
+      } else if (day > 21 && day <= 30) {
         this.value = 'Cancer';
+      } else {
+        this.value = 'Only 30 days in June!';
       }
     } else if (month === 'july') {
       if (day < 23) {
         this.value = 'Cancer';
-      } else {
+      } else if (day > 23 && day <= 31) {
         this.value = 'Leo';
+      } else {
+        this.value = 'Only 31 days in July!';
       }
     } else if (month === 'august') {
       if (day < 23) {
         this.value = 'Leo';
-      } else {
+      } else if (day > 23 && day <= 31) {
         this.value = 'Virgo';
+      } else {
+        this.value = 'Only 31 days in August!';
       }
     } else if (month === 'september') {
       if (day < 23) {
         this.value = 'Virgo';
-      } else {
+      } else if (day > 23 && day <= 30) {
         this.value = 'Libra';
+      } else {
+        this.value = 'Only 30 days in September!';
       }
     } else if (month === 'october') {
       if (day < 23) {
         this.value = 'Libra';
-      } else {
+      } else if (day > 23 && day <= 31) {
         this.value = 'Scorpio';
+      } else {
+        this.value = 'Only 31 days in October!';
       }
     } else if (month === 'november') {
       if (day < 22) {
-        this.value = 'Scorpio';
-      } else {
+        this.value = 'scorpio';
+      } else if (day > 22 && day <= 30) {
         this.value = 'Sagittarius';
+      } else {
+        this.value = 'Only 30 days in November!';
       }
+    } else {
+      this.value = 'Nada! Either the month or day is wrong!';
     }
     return astroSign;
   }
@@ -125,193 +147,15 @@ export class ZodiacSignsComponent implements OnInit {
     map.set('Melvin', this.zodiac('june', 16));
     map.set('Angeles', this.zodiac('august', 21));
 
-    // Iterate over map keys
-    // for (let key of map.keys()) {
-    //    console.log(key);                   //A B C
-    // }
-
-    // Iterate over map values
-    // for (let value of map.values()) {
-    //     console.log(value);                 //1 2 3
-    // }
-
     // Iterate over map entries
     for (const entry of map.entries()) {
-      console.log(entry[0], entry[1]);    // 'A' 1 'B' 2 'C' 3
+      // console.log(entry[0], entry[1]);
     }
 
-    // Using object destructuring
-    // for (let [key, value] of map) {
-    //     console.log(key, value);            //'A' 1 'B' 2 'C' 3
-    // }
-
   }
-
-  ZodiacCalculate() {
-    // this.zodiac(month, day);
-
-    //  alert('Placeholder, display Zodiac function result here');
-  }
-
-
 
 }
 
 
-
-// import { Component, OnInit } from '@angular/core';
-// import { analyzeAndValidateNgModules } from '@angular/compiler';
-// import { MonthPickerComponent } from 'ngx-bootstrap';
-// import { Http } from '@angular/http';
-// interface Izodiac {
-//   name: string;
-//   month: string;
-//   day: number;
-//   zodiac?: string;
-// }
-// @Component({
-//   selector: 'app-zodiac-signs',
-//   templateUrl: './zodiac-signs.component.html',
-//   styleUrls: ['./zodiac-signs.component.css']
-// })
-
-// export class ZodiacSignsComponent implements OnInit {
-//   const zodiac: string;
-//   public zod: ZodiacSignsComponent;
-//   zodiacs: Array<Izodiac> = [];
-//   nameInput = ' ';
-
-//   date = '';
-//   constructor() {
-//   }
-
-//   ngOnInit() {
-
-//   }
-
-
-
-//   zodiac(month, day) {
-//     let astroSign = '';
-//     alert(astroSign);
-//     checks month and date within the
-//     valid range of a specified zodiac
-//     if (month === 'december') {
-
-//       if (day < 22) {
-//         astroSign = 'Sagittarius';
-//       } else {
-//         astroSign = 'capricorn';
-//       }
-//     } else if (month === 'january') {
-//       if (day < 20) {
-//         astroSign = 'Capricorn';
-//       } else {
-//         astroSign = 'aquarius';
-//       }
-//     } else if (month === 'february') {
-//       if (day < 19) {
-//         astroSign = 'Aquarius';
-//       } else {
-//         astroSign = 'pisces';
-//       }
-//     } else if (month === 'march') {
-//       if (day < 21) {
-//         astroSign = 'Pisces';
-//       } else {
-//         astroSign = 'aries';
-//       }
-//     } else if (month === 'april') {
-//       if (day < 20) {
-//         astroSign = 'Aries';
-//       } else {
-//         astroSign = 'taurus';
-//       }
-//     } else if (month === 'may') {
-//       if (day < 21) {
-//         astroSign = 'Taurus';
-//       } else {
-//         astroSign = 'gemini';
-//       }
-//     } else if (month === 'june') {
-//       if (day < 21) {
-//         astroSign = 'Gemini';
-//       } else {
-//         astroSign = 'cancer';
-//       }
-//     } else if (month === 'july') {
-//       if (day < 23) {
-//         astroSign = 'Cancer';
-//       } else {
-//         astroSign = 'leo';
-//       }
-//     } else if (month === 'august') {
-//       if (day < 23) {
-//         astroSign = 'Leo';
-//       } else {
-//         astroSign = 'virgo';
-//       }
-//     } else if (month === 'september') {
-//       if (day < 23) {
-//         astroSign = 'Virgo';
-//       } else {
-//         astroSign = 'libra';
-//       }
-//     } else if (month === 'october') {
-//       if (day < 23) {
-//         astroSign = 'Libra';
-//       } else {
-//         astroSign = 'scorpio';
-//       }
-//     } else if (month === 'november') {
-//       if (day < 22) {
-//         astroSign = 'scorpio';
-//       } else {
-//         astroSign = 'sagittarius';
-//       }
-//     }
-
-//     return astroSign;
-
-//   }
-
-//   doSomething() {
-//     let map = new Map();
-//     map.set('Ali', this.zodiac('december', 25));
-//     map.set('Alejandra', this.zodiac('september', 26));
-//     map.set('Melvin', this.zodiac('june', 16));
-//     map.set('Angeles', this.zodiac('august', 21));
-
-//     Iterate over map keys
-//     for (let key of map.keys()) {
-//        console.log(key);                   A B C
-//     }
-
-//     Iterate over map values
-//     for (let value of map.values()) {
-//         console.log(value);                 1 2 3
-//     }
-
-//     Iterate over map entries
-//     for (let entry of map.entries()) {
-//       console.log(entry[0], entry[1]);     'A' 1 'B' 2 'C' 3
-//     }
-
-//     Using object destructuring
-//     for (let [key, value] of map) {
-//         console.log(key, value);            'A' 1 'B' 2 'C' 3
-//     }
-
-//   }
-
-//   ZodiacCalculate() {
-//     this.zodiac(month, day);
-
-//     alert('Placeholder, display Zodiac function result here');
-//   }
-
-
-
-// }
 
 
